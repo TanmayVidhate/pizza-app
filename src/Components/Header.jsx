f34704cd360ae4209980da38766d7a8e4d377d3a
tanmay-vidhate <tanmayvidhate7@gmail.com>
2025-07-01 11:04:32 +0530

    add header

---
+++ src/Components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./index.css"
+export default function Header() {
+    return (
+        <>
+            <header className='header'>
+                <h1>Fast React Pizaa Co.</h1>
+            </header>
+        </>
+    )
+}
